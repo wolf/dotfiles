cf4c09357424c34293ca47f28552b86086dae74e
Wolf <wolf@zv.cx>
2017-03-22 07:10:18 -0400

    improve cdw
    allow an argument for a relative jump, so
    
        cdw
    
    still takes you to $WORKSPACE, but
    
        cdw config/local
    
    takes you to the specified sub-dir inside $WORKSPACE.

--- bash/.bash_topics.d/laz.bash
+++ bash/.bash_topics.d/laz.bash
@@ -1,10 +1,12 @@
 if [[ $(uname) =~ CYGWIN.* ]] ; then
     export WORKSPACE=/cygdrive/c/kepler/workspace
     export WINDOWSHOME=/cygdrive/c/Users/wolfe
-    alias cdw='cd $WORKSPACE'
     alias make-tags="$WORKSPACE/tools/scripts/shell/make-tags.sh"
-
     alias fix-wrapping='kill -WINCH $$'
+
+    function cdw() {
+        cd "$WORKSPACE/$1"
+    }
 else
     function be() {
         sudo -u "$1" -i bash;
