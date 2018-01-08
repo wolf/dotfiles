dc270ac2e018bbf66cdd85a19158c2416233fa80
Wolf <wolf@zv.cx>
2018-01-08 08:57:51 -0500

    more directory shortcuts

--- bash/.bash_topics.d/laz.bash
+++ bash/.bash_topics.d/laz.bash
@@ -1,6 +1,9 @@
 if [[ $(uname) =~ CYGWIN.* ]] ; then
     export WORKSPACE=/cygdrive/c/kepler/workspace
-    export WINDOWSHOME=/cygdrive/c/Users/wolf
+    export WINDOWSHOME=/cygdrive/c/Users/Wolf
+    export INVESTIGATION=/cygdrive/d/investigate_content
+    export PLANNING=~/planning
+
     alias make-tags="$WORKSPACE/tools/scripts/shell/make-tags.sh"
     alias fix-wrapping='kill -WINCH $$'
 
@@ -10,6 +13,18 @@ if [[ $(uname) =~ CYGWIN.* ]] ; then
 
     function cdw() {
         cd "$WORKSPACE/$1"
+    }
+
+    function cdh() {
+        cd "$WINDOWSHOME/$1"
+    }
+
+    function cdi() {
+        cd "$INVESTIGATION/$1"
+    }
+
+    function cdp() {
+        cd "$PLANNING/$1"
     }
 else
     function be() {
