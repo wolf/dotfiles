9d42e5532d28e4c20ee98c6b5f07ae972a1db0e3
Wolf <wolf@zv.cx>
2019-03-18 10:42:27 -0400

    add bat, pyenv, and PATH to include .local

---
+++ bash/.bash_topics.d/bat.bash
@@ -0,0 +1,15 @@
+if [[ $(uname) =~ CYGWIN.* ]] ; then
+    export BAT_CONFIG_PATH="$(cygpath --windows ~/.config/bat/config)"
+
+    function bat() {
+        local index
+        local args=("$@")
+        for index in $(seq 0 ${#args[@]}) ; do
+            case "${args[index]}" in
+                -*) continue ;;
+                *)  [ -e "${args[index]}"  ] && args[index]="$(cygpath --windows "${args[index]}")" ;;
+            esac
+        done
+        command bat "${args[@]}"
+    }
+fi
