a3daf10020c0c3d1558eb546e222d40edc3d85ca
Wolf <wolf@zv.cx>
2019-03-18 11:08:36 -0400

    I don't use /home/wolf/planning, etc anymore

--- bash/.bash_topics.d/laz.bash
+++ bash/.bash_topics.d/laz.bash
@@ -1,8 +1,6 @@
 if [[ $(uname) =~ CYGWIN.* ]] ; then
     export WORKSPACE=/cygdrive/c/kepler/workspace
     export WINDOWSHOME=/cygdrive/c/Users/Wolf
-    export INVESTIGATION=/cygdrive/d/investigate_content
-    export PLANNING=~/planning
 
     alias make-tags="$WORKSPACE/tools/scripts/shell/make-tags.sh"
     alias fix-wrapping='kill -WINCH $$'
@@ -17,14 +15,6 @@ if [[ $(uname) =~ CYGWIN.* ]] ; then
 
     function cdh() {
         cd "$WINDOWSHOME/$1"
-    }
-
-    function cdi() {
-        cd "$INVESTIGATION/$1"
-    }
-
-    function cdp() {
-        cd "$PLANNING/$1"
     }
 else
     function be() {
