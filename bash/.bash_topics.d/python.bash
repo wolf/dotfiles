5386092f5af138139cac040a1ca1058cca9a8c40
Wolf <wolf@learninga-z.com>
2019-03-21 12:38:43 -0400

    use flake8 instead of pyflakes and pylint

--- bash/.bash_topics.d/python.bash
+++ bash/.bash_topics.d/python.bash
@@ -1,15 +1,7 @@
-if [ $(command -v pyflakes) ]; then
-    # only define pyflakes commands if pyflakes is available
-    function pyflakes_name()    { find . -name $1 | xargs pyflakes; }
-    function pyflakes_since()   { since_commit $1 | grep '\.py$' | xargs pyflakes; }
-    function pyflakes_commit()  { in_commit $1 | grep '\.py$' | xargs pyflakes; }
-    function pyflakes_dirty()   { dirty | grep '\.py$' | xargs pyflakes; }
-fi
-
-if [ $(command -v pylint) ]; then
-    # only define pylint commands if pylint is available
-    function pylint_name()      { find . -name $1 | xargs pylint; }
-    function pylint_since()     { since_commit $1 | grep '\.py$' | xargs pylint; }
-    function pylint_commit()    { in_commit $1 | grep '\.py$' | xargs pylint; }
-    function pylint_dirty()     { dirty | grep '\.py$' | xargs pylint; }
+if [ $(command -v flake8) ]; then
+    # only define flake8 commands if flake8 is available
+    function flake8_name()      { find . -name $1 | xargs flake8; }
+    function flake8_since()     { since_commit $1 | grep '\.py$' | xargs flake8; }
+    function flake8_commit()    { in_commit $1 | grep '\.py$' | xargs flake8; }
+    function flake8_dirty()     { dirty | grep '\.py$' | xargs flake8; }
 fi
