c339db1f855a00c767b8883ac52741e9bfc96623
Wolf <wolf@zv.cx>
2019-04-11 20:58:07 -0400

    oops.  drop error messages from cygpath

--- bash/.bash_topics.d/jetbrains.bash
+++ bash/.bash_topics.d/jetbrains.bash
@@ -1,12 +1,12 @@
 if [[ $(uname) =~ .*_NT-10.0 ]] ; then
-    IDEA_DIR=$(cygpath --unix "$LOCALAPPDATA/JetBrains/Toolbox/apps/IDEA-U")
-    CHARM_DIR=$(cygpath --unix "$LOCALAPPDATA/JetBrains/Toolbox/apps/PyCharm-P")
+    IDEA_DIR=$(cygpath --unix "$LOCALAPPDATA/JetBrains/Toolbox/apps/IDEA-U" 2>/dev/null)
+    CHARM_DIR=$(cygpath --unix "$LOCALAPPDATA/JetBrains/Toolbox/apps/PyCharm-P" 2>/dev/null)
     IDEA=$(find "${IDEA_DIR}" -name idea64.exe 2>/dev/null)
     CHARM=$(find "${CHARM_DIR}" -name pycharm64.exe 2>/dev/null)
 
     case $(uname -o) in
-        Msys)   alias idea=$(cygpath --mixed "${IDEA}")
-                alias charm=$(cygpath --mixed "${CHARM} &")
+        Msys)   alias idea=$(cygpath --mixed "${IDEA}" 2>/dev/null)
+                alias charm="$(cygpath --mixed "${CHARM}" 2>/dev/null) &"
                 ;;
         Cygwin) alias idea="${IDEA}"
                 alias charm="${CHARM} &"
