feee139000b220429553ea5854807a405b52deeb
Wolf <wolf@zv.cx>
2020-11-30 10:43:11 -0500

    Add functions to automatically activate venvs on cd

---
+++ bash/.bash_topics.d/python-virtualenvs.bash
@@ -0,0 +1,18 @@
+function activate_found_venv() {
+    if [[ ! -z "$VIRTUAL_ENV" ]] ; then
+        deactivate
+    fi
+    if [ -d .venv ] ; then
+        VENV_DIR=.venv
+    else
+        VENV_DIR=$(ls -d *.venv 2>/dev/null | awk 'BEGIN { getline; print }')
+    fi
+    if [[ ! -z "$VENV_DIR" ]] ; then
+        source ./$VENV_DIR/bin/activate
+    fi
+}
+
+function cdvenv() {
+    cd "$1"
+    activate_found_venv
+}
