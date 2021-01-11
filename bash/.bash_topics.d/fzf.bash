b1777e978e565f374535922eb41a3fe00ea9c59f
Wolf <wolf@zv.cx>
2021-01-11 07:17:00 -0500

    Don't let fzf look in .idea folders, either
    ...or __pycache__ or *.venv folders or at .DS_Store

--- bash/.bash_topics.d/fzf.bash
+++ bash/.bash_topics.d/fzf.bash
@@ -1,1 +1,1 @@
-export FZF_DEFAULT_COMMAND='find . -type f | grep -vE "(node_modules|\.git|.*\.swp)"'
+export FZF_DEFAULT_COMMAND='find . -type f | grep -vE "(node_modules|__pycache__|\.idea|\.git|.*\.venv|.*\.swp|\.DS_Store)"'
