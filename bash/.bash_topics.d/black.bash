95e266035e1a081c5427fe5230917318f38d9ad2
Wolf <wolf@zv.cx>
2021-11-28 17:04:18 -0500

    Remove extra spaces

--- bash/.bash_topics.d/black.bash
+++ bash/.bash_topics.d/black.bash
@@ -1,4 +1,4 @@
-function black_name()       { find . -name "$1" | xargs  black; }
-function black_since()      { since_commit "$1" | grep '\.py$' | xargs  black; }
-function black_commit()     { in_commit "$1" | grep '\.py$' | xargs  black; }
-function black_dirty()      { dirty | grep '\.py$' | xargs  black; }
+function black_name()       { find . -name "$1" | xargs black; }
+function black_since()      { since_commit "$1" | grep '\.py$' | xargs black; }
+function black_commit()     { in_commit "$1" | grep '\.py$' | xargs black; }
+function black_dirty()      { dirty | grep '\.py$' | xargs black; }
