abdcf5a221934be157ed7db92fa2fa550405af85
Wolf <wolf@zv.cx>
2021-11-29 09:46:47 -0500

    Use fd if available in black, flake8, and jshint

--- bash/.bash_topics.d/black.bash
+++ bash/.bash_topics.d/black.bash
@@ -1,4 +1,13 @@
-function black_name()       { find . -name "$1" | xargs black; }
+if [ "$(command -v fd)" ] ; then
+    function black_name() {
+        fd --type f --extension py --glob --follow --hidden "$@" -X black
+    }
+else
+    function black_name() {
+        find . -name "$1" --print0 | xargs -0 black;
+    }
+fi
+
 function black_since()      { since_commit "$1" | grep '\.py$' | xargs black; }
 function black_commit()     { in_commit "$1" | grep '\.py$' | xargs black; }
 function black_dirty()      { dirty | grep '\.py$' | xargs black; }
--- bash/.bash_topics.d/jshint.bash
+++ bash/.bash_topics.d/jshint.bash
@@ -1,7 +1,17 @@
-if [ $(command -v jshint) ]; then
+if [ "$(command -v jshint)" ]; then
     # only define jshint commands if jshint is available
-    function jshint_name()      { find . -name $1 | xargs jshint; }
-    function jshint_since()     { since_commit $1 | grep '\.js$' | xargs jshint; }
-    function jshint_commit()    { in_commit $1 | grep '\.js$' | xargs jshint; }
+
+    if [ "$(command -v fd)" ] ; then
+        function jshint_name() {
+            fd --type f --extension js --glob --follow --hidden "$@" -X jshint
+        }
+    else
+        function jshint_name() {
+            find . -name "$1" --print0 | xargs -0 jshint;
+        }
+    fi
+
+    function jshint_since()     { since_commit "$1" | grep '\.js$' | xargs jshint; }
+    function jshint_commit()    { in_commit "$1" | grep '\.js$' | xargs jshint; }
     function jshint_dirty()     { dirty | grep '\.js$' | xargs jshint; }
 fi
