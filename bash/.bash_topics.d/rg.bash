28cacfe7afb5ca845d5a043606e205c3c8d9c55a
Wolf <wolf@zv.cx>
2021-12-23 14:50:39 -0500

    Configure ripgrep

--- bash/.bash_topics.d/rg.bash
+++ bash/.bash_topics.d/rg.bash
@@ -1,5 +1,7 @@
 command -v rg >/dev/null || return
 
+export RIPGREP_CONFIG_PATH=~/.config/rg/config
+
 function ge() {
     # shellcheck disable=SC2086
     rg --hidden --files-with-matches "$@" | xargs -o ${EDITOR}
