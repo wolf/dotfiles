b7975bc54820bd4a4745bce0b1926a4bc79afaf0
Wolf <wolf@zv.cx>
2021-12-23 10:24:31 -0500

    Clean up and refactor python-virtualenvs Bash topic

--- bash/.bash_topics.d/python-virtualenvs.bash
+++ bash/.bash_topics.d/python-virtualenvs.bash
@@ -1,40 +1,57 @@
 export VIRTUAL_ENV_DISABLE_PROMPT=1
 
 function virtualenv_info() {
-    [ "${VIRTUAL_ENV}" ] && echo ' ('$(basename "${VIRTUAL_ENV}")')';
+    [ -v VIRTUAL_ENV ] && echo ' ('"$(basename "${VIRTUAL_ENV}")"')';
 }
 
-# TODO: every place here I use bin/activate I probably need to test and possibly use Scripts/activate
-
-function create_venv() {
-    if [ -v VIRTUAL_ENV ] ; then
+function deactivate_venv() {
+    [ -v VIRTUAL_ENV ] || return
+    if [ -v PYENV_VIRTUALENV_INIT ] ; then
+        # shellcheck disable=SC1091
+        source deactivate 2>/dev/null
+    else
         deactivate
     fi
-    if [[ -z "${1+x}" ]] ; then
+}
+
+function activate_venv() {
+    VENV_TO_ACTIVATE="${1}"
+    [ -z "${VENV_TO_ACTIVATE}" ] && return
+    deactivate_venv
+
+    if [ -d "${VENV_TO_ACTIVATE}/bin" ] ; then
+        # shellcheck disable=SC1091
+        source "${VENV_TO_ACTIVATE}/bin/activate"
+    elif [ -d "${VENV_TO_ACTIVATE}/Scripts" ] ; then
+        # shellcheck disable=SC1091
+        source "${VENV_TO_ACTIVATE}/Scripts/activate"
+    fi
+}
+
+function create_venv() {
+    deactivate_venv
+    if [ -z "${1}" ] ; then
         VENV_DIR="$(basename "$(pwd)")".venv
     else
-        mkdir -p "$(dirname "$1")"
-        VENV_DIR="$1"
+        mkdir -p "$(dirname "${1}")"
+        VENV_DIR="${1}"
     fi
-    python -m venv --copies --upgrade-deps "$VENV_DIR"
-    # shellcheck disable=SC1091
-    source "${VENV_DIR}/bin/activate"
+    python -m venv --copies --upgrade-deps "${VENV_DIR}"
+    activate_venv "${VENV_DIR}"
     python -m pip install --upgrade wheel
+    if [ -n "${2}" ] && [ -f "${2}" ] ; then
+        python -m pip install -r "${2}"
+    fi
 }
 
 function activate_found_venv() {
-    if [ -v VIRTUAL_ENV ] ; then
-        deactivate
-    fi
+    deactivate_venv
     if [ -d .venv ] ; then
         VENV_DIR=.venv
     else
-        VENV_DIR=$(command ls --color=never -d ./*.venv 2>/dev/null | head -n 1)
+        VENV_DIR=$(command ls --color=never -d ./*venv 2>/dev/null | head -n 1)
     fi
-    if [[ -n "${VENV_DIR}" ]] ; then
-        # shellcheck disable=SC1090
-        source ./"${VENV_DIR}"/bin/activate
-    fi
+    activate_venv "${VENV_DIR}"
 }
 
 function rename_venv() {
@@ -43,10 +60,9 @@ function rename_venv() {
     RANDOM_STRING=$(python -c "import uuid; print(str(uuid.uuid4()),end='')")
     TEMP_FILE_NAME=/tmp/${RANDOM_STRING}.requirements.txt
 
-    # shellcheck disable=SC1091
-    source "${FROM_NAME}/bin/activate"
+    activate_venv "${FROM_NAME}"
     python -m pip freeze > "${TEMP_FILE_NAME}"
-    deactivate 2>/dev/null
+    deactivate_venv
 
     create_venv "${TO_NAME}"
     python -m pip install -r "${TEMP_FILE_NAME}"
@@ -57,7 +73,7 @@ function rename_venv() {
 }
 
 function cdv() {
-    cd "$1" || return
+    cd "${1}" || return
     activate_found_venv
 }
 
