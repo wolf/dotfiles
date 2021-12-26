0ed9ce61d168c9c75d163f486b204165d279c708
Wolf <wolf@zv.cx>
2021-12-26 08:44:10 -0500

    Add a function to _automatically_ generate help
    Using a comment that ends the line defining a function or alias name,
    anywhere inside $DOTFILES_DIR/bash.

--- bash/.bash_topics.d/ripgrep.bash
+++ bash/.bash_topics.d/ripgrep.bash
@@ -6,3 +6,11 @@ function ge() {
     # shellcheck disable=SC2086
     rg --hidden --files-with-matches "$@" | xargs -o ${EDITOR}
 }
+
+
+function help() { # help : you're soaking in it!
+    rg --hidden --color=never --no-line-number \
+        -e '\s*(?:function|alias)\s+([a-z][a-z0-9_]*).*(#.*)' \
+        --replace "\$1		\$2" \
+        "${DOTFILES_DIR}/bash"
+}
