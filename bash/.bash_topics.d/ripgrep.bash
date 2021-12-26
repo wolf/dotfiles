445e28746c800b8d7ace0040cac4ac6007bc6611
Wolf <wolf@zv.cx>
2021-12-26 11:28:07 -0500

    Use rg (if we have it) when you accidentally type grep

--- bash/.bash_topics.d/ripgrep.bash
+++ bash/.bash_topics.d/ripgrep.bash
@@ -2,11 +2,12 @@ command -v rg >/dev/null || return
 
 export RIPGREP_CONFIG_PATH=~/.config/rg/config
 
+alias grep="rg"
+
 function ge() { # ge <pattern> : find files in or below . whose contents somehow match pattern, and open them in $EDITOR
     # shellcheck disable=SC2086
     rg --hidden --files-with-matches "$@" | xargs -o ${EDITOR}
 }
-
 
 function help_commands() { # help : you're soaking in it!
     rg --hidden --color=never --no-line-number --heading \
