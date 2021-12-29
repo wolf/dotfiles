c86e3f5d34bbad1422d863f135eb2a858662f75a
Wolf <wolf@zv.cx>
2021-12-29 16:48:33 -0500

    Ugh.  Fix path-separator direction

--- bash/.bash_topics.d/mingw/ripgrep.bash
+++ bash/.bash_topics.d/mingw/ripgrep.bash
@@ -2,5 +2,5 @@ command -v rg >/dev/null || return
 
 function ge() { # ge <pattern> : find files in or below . whose contents somehow match pattern, and open them in $EDITOR
     # shellcheck disable=SC2086
-    rg --follow --hidden --files-with-matches --path-separator="\\" "$@" | xargs -o ${EDITOR}
+    rg --follow --hidden --files-with-matches --path-separator="//" "$@" | xargs -o ${EDITOR}
 }
