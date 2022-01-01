37163450bbd2193a98e0a40669589c01f759186c
Wolf <wolf@zv.cx>
2022-01-01 15:50:53 -0500

    Drop some broken and unused Git functions

--- bash/.bash_topics.d/git.bash
+++ bash/.bash_topics.d/git.bash
@@ -34,35 +34,8 @@ function pushdtop() { # usage: pushdtop [<relative-path>] : pushd combined with 
     fi
 }
 
-function since_commit() { # since_commit [<ref>] : list all the files modified by all the commits since the given <ref>, including currently unstaged changes
-    # example: since_commit
-    # example: since_commit 12345
-    # example: since_commit HEAD~3
-    local COMMIT="${1:-HEAD}" ; shift
-    git diff "${COMMIT}" --name-only --relative "$@"
-}
-
-function in_commit() { # in_commit [<ref>] : list all the files modified as part of the given commit
-    # example: in_commit
-    # example: in_commit 12345
-    # example: in_commit HEAD~3
-    local FROM_COMMIT="${1:-HEAD}" ; shift
-    local TO_COMMIT="${1:-${FROM_COMMIT}}" ; shift
-    git diff "${FROM_COMMIT}" "${TO_COMMIT}^" --name-only --relative "$@"
-}
-
 function dirty() { # dirty : list currently modified and/or unmerged files that exist in this repo
     git ls-files --modified --deduplicate "$@"
-}
-
-function edit_since() { # edit_since [<ref>] : like since_commit, but open in $EDITOR instead of list
-    # shellcheck disable=SC2086
-    since_commit "$@" -z | xargs -o -0 ${EDITOR}
-}
-
-function edit_commit() { # edit_commit [<ref>] : like in_commit, but open in $EDITOR instead of list
-    # shellcheck disable=SC2086
-    in_commit "$@" -z | xargs -o -0 ${EDITOR}
 }
 
 function edit_dirty() { # edit_dirty : like dirty, but open in $EDITOR instead of list
@@ -70,15 +43,15 @@ function edit_dirty() { # edit_dirty : like dirty, but open in $EDITOR instead o
     dirty "$@" -z | xargs -o -0 ${EDITOR}
 }
 
-function zadd() {
+function zadd() { # zadd : brings up multi-select fzf over the set of files you can add.  Choose whichever ones you want, and they are added
     git ls-files --modified --others --exclude-standard --deduplicate -z \
         | fzf --read0 --print0 --multi --preview-window='60%' --preview='bat --color=always --style=header,changes,grid {}' \
         | xargs -0 git add
 }
 
-function zshow() { # fzf_git_show [git-log-options...] : brings up fzf over a log of selected commits, then shows the chosen one
-    # example: fzf_git_show
-    # example: fzf_git_show --author=Wolf --since="{2 days ago}"
+function zshow() { # zshow [git-log-options...] : brings up fzf over a log of selected commits, then shows the chosen one
+    # example: zshow
+    # example: zshow --author=Wolf --since="{2 days ago}"
     local COMMIT_TO_SHOW
 
     COMMIT_TO_SHOW="$(git log --abbrev-commit --oneline "$@" | fzf | cut -d ' ' -f 1)"
