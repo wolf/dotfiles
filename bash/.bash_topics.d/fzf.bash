d33bbbefa9d2615bed6e3a0f7d509c7c4e78a3f6
Wolf <wolf@zv.cx>
2022-01-01 11:25:15 -0500

    Rework the underly scheme of my simple Git tools
    Now they use xargs and also forward arguments into the underyling git
    calls.
    
    Added zadd, a fuzzy implementation of git add.  And adjusted the global
    options for FZF so it's top-down instead of bottom-up.  That makes the
    keys easier to use in --multi mode, which zadd uses.
    
    Renamed fzf_git_show to zshow.  From now on, all my FZF-based tools will
    start with a 'z'

--- bash/.bash_topics.d/fzf.bash
+++ bash/.bash_topics.d/fzf.bash
@@ -3,7 +3,7 @@ command -v fzf >/dev/null || return
 # shellcheck disable=SC1090
 [ -f ~/.fzf.bash ] && source ~/.fzf.bash
 
-export FZF_DEFAULT_OPTS='--ansi'
+export FZF_DEFAULT_OPTS='--ansi --layout=reverse'
 export FZF_DEFAULT_COMMAND='fd --follow --hidden --type file --color=always'
 
 function fze() { # fze [<pattern>] : use fzf to select one or more files in or below . whose names match <pattern> and open them in $EDITOR
--- bash/.bash_topics.d/git.bash
+++ bash/.bash_topics.d/git.bash
@@ -38,27 +38,45 @@ function since_commit() { # since_commit [<ref>] : list all the files modified b
     # example: since_commit
     # example: since_commit 12345
     # example: since_commit HEAD~3
-    local COMMIT="${1:-HEAD}"
-    git diff "${COMMIT}" --name-only --relative
+    local COMMIT="${1:-HEAD}" ; shift
+    git diff "${COMMIT}" --name-only --relative "$@"
 }
 
 function in_commit() { # in_commit [<ref>] : list all the files modified as part of the given commit
     # example: in_commit
     # example: in_commit 12345
     # example: in_commit HEAD~3
-    local COMMIT="${1:-HEAD}"
-    git diff "${COMMIT}" "${COMMIT}^" --name-only --relative
+    local FROM_COMMIT="${1:-HEAD}" ; shift
+    local TO_COMMIT="${1:-${FROM_COMMIT}}" ; shift
+    git diff "${FROM_COMMIT}" "${TO_COMMIT}^" --name-only --relative "$@"
 }
 
 function dirty() { # dirty : list currently modified and/or unmerged files that exist in this repo
-    git ls-files --modified | sort -u
+    git ls-files --modified --deduplicate "$@"
 }
 
-function edit_since()   { ${EDITOR} $(since_commit "${1}"); }       # edit_since [<ref>] : like since_commit, but open in $EDITOR instead of list
-function edit_commit()  { ${EDITOR} $(in_commit "${1}"); }          # edit_commit [<ref>] : like in_commit, but open in $EDITOR instead of list
-function edit_dirty()   { ${EDITOR} $(dirty); }                     # edit_dirty : like dirty, but open in $EDITOR instead of list
+function edit_since() { # edit_since [<ref>] : like since_commit, but open in $EDITOR instead of list
+    # shellcheck disable=SC2086
+    since_commit "$@" -z | xargs -o -0 ${EDITOR}
+}
 
-function fzf_git_show() { # fzf_git_show [git-log-options...] : brings up fzf over a log of selected commits, then shows the chosen one
+function edit_commit() { # edit_commit [<ref>] : like in_commit, but open in $EDITOR instead of list
+    # shellcheck disable=SC2086
+    in_commit "$@" -z | xargs -o -0 ${EDITOR}
+}
+
+function edit_dirty() { # edit_dirty : like dirty, but open in $EDITOR instead of list
+    # shellcheck disable=SC2086
+    dirty "$@" -z | xargs -o -0 ${EDITOR}
+}
+
+function zadd() {
+    git ls-files --modified --others --exclude-standard --deduplicate -z \
+        | fzf --read0 --print0 --multi --preview-window='60%' --preview='bat --color=always --style=header,changes,grid {}' \
+        | xargs -0 git add
+}
+
+function zshow() { # fzf_git_show [git-log-options...] : brings up fzf over a log of selected commits, then shows the chosen one
     # example: fzf_git_show
     # example: fzf_git_show --author=Wolf --since="{2 days ago}"
     local COMMIT_TO_SHOW
