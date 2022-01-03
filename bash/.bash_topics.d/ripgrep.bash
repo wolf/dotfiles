cf05761efaa466883edc8ae675fa7460cd051cbd
Wolf <wolf@zv.cx>
2022-01-03 08:43:23 -0500

    Add a couple more RipGrep-based functions
    One, g, is to be used exactly like rg or grep.  It's a simple search.
    The other, gv, which is only defined for Vim users, does a search and
    then opens up the result in a new instance of Vim in the quickfix list.

--- bash/.bash_topics.d/ripgrep.bash
+++ bash/.bash_topics.d/ripgrep.bash
@@ -2,9 +2,14 @@ command -v rg >/dev/null || return
 
 export RIPGREP_CONFIG_PATH=~/.config/rg/config
 
-alias grep="rg"
+function g() { # g <pattern> : find matches in files in or below .
+    # shellcheck disable=SC2086
+    rg --follow --hidden --smart-case --heading "$@"
+}
+
+alias grep='g'
 
 function ge() { # ge <pattern> : find files in or below . whose contents somehow match pattern, and open them in $EDITOR
     # shellcheck disable=SC2086
-    rg --follow --hidden --files-with-matches "$@" | xargs -o ${EDITOR}
+    rg --follow --hidden --smart-case --files-with-matches "$@" | xargs -o ${EDITOR}
 }
