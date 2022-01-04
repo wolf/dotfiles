49c1da226edbafeccbe40d198d4a06f8ff12bd7d
Wolf <wolf@zv.cx>
2022-01-04 06:39:01 -0500

    Handle more Windows broken-ness

--- bash/.bash_topics.d/mingw/fd.bash
+++ bash/.bash_topics.d/mingw/fd.bash
@@ -4,6 +4,11 @@ function fcat() { # fcat <pattern> : find all the files in or below . whose name
     fd --follow --no-ignore --hidden --glob --type f "$@" --exec-batch cat
 }
 
+function fe() { # fe <pattern> : find all the files in or below . whose names match the given pattern and open them in $EDITOR
+    # shellcheck disable=SC2086
+    fd --follow --no-ignore --hidden --glob --type f "$@" --path-separator="//" --print0 | xargs -o -0 ${EDITOR}
+}
+
 function fll() { # fll <pattern> : find all the files in or below . whose names match the given pattern, and list them as would ls -l
     fd --follow --no-ignore --hidden --list-details --glob "$@"
 }
