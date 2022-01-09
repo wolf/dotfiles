330b154bdae9955f72cf7b1753b2d3fa89eef190
Wolf <wolf@zv.cx>
2022-01-09 05:23:16 -0500

    Teach f and friends to exclude the usual suspects
    In hindsight, --no-ignore was probably too aggressive.  Let's try for a
    while without it.

--- bash/.bash_topics.d/fd.bash
+++ bash/.bash_topics.d/fd.bash
@@ -1,11 +1,11 @@
 command -v fd >/dev/null || return
 
-function f() { # f <pattern> : list all the files in or below . whose names match <pattern>.  You can give options to fd with all f-commands
-    fd --follow --no-ignore --hidden --glob "$@" 2>/dev/null
+function f() { # f <pattern> : list all the files in or below . whose names match <pattern>.  You can give options to fd with all the f-commands
+    fd --follow --hidden --glob --exclude ".git" "$@" 2>/dev/null
 }
 
 function fcat() { # fcat <pattern> : find all the files in or below . whose names match <pattern> and cat them
-    fd --follow --no-ignore --hidden --glob --type f "$@" --exec-batch bat
+    fd --follow --hidden --glob --type f "$@" --exec-batch bat
 }
 
 function fcd() { # fcd <pattern> : find the first directory in or below . whose name matches <pattern> and cd into it
@@ -14,7 +14,7 @@ function fcd() { # fcd <pattern> : find the first directory in or below . whose 
     # example: fcd --regex 'venv$'
     local FIRST_MATCHING_DIRECTORY
 
-    FIRST_MATCHING_DIRECTORY="$(fd --follow --no-ignore --hidden --glob --type d --max-results 1 "$@" 2>/dev/null)"
+    FIRST_MATCHING_DIRECTORY="$(fd --follow --hidden --glob --type d --max-results 1 "$@" 2>/dev/null)"
     if [ -d "${FIRST_MATCHING_DIRECTORY}" ]; then
         cd "${FIRST_MATCHING_DIRECTORY}" || return
     fi
@@ -24,7 +24,7 @@ function fcd_of { # fcd_of <pattern> : find the first file-system object in or b
     local FIRST_MATCH
     local PARENT_OF_FIRST_MATCH
 
-    FIRST_MATCH="$(fd --follow --no-ignore --hidden --glob --max-results 1 "$@" 2>/dev/null)"
+    FIRST_MATCH="$(fd --follow --hidden --glob --max-results 1 "$@" 2>/dev/null)"
     PARENT_OF_FIRST_MATCH="$(dirname "${FIRST_MATCH}")"
 
     if [ -d "${PARENT_OF_FIRST_MATCH}" ]; then
@@ -34,17 +34,17 @@ function fcd_of { # fcd_of <pattern> : find the first file-system object in or b
 
 function fe() { # fe <pattern> : find all the files in or below . whose names match <pattern> and open them in $EDITOR
     # shellcheck disable=SC2086
-    fd --follow --no-ignore --hidden --glob --type f "$@" --exec-batch ${EDITOR}
+    fd --follow --hidden --glob --type f "$@" --exec-batch ${EDITOR}
 }
 
 function fll() { # fll <pattern> : find all the files in or below . whose names match <pattern>, and list them as would ls -l
-    fd --follow --no-ignore --hidden --glob "$@" --exec-batch exa -Fal
+    fd --follow --hidden --glob "$@" --exec-batch exa -Fal
 }
 
 function fsource() { # fsource <pattern> : find all the files in or below . whose names match <pattern> and source them
     local FILE FILES
     declare -a FILES
-    readarray -d '' FILES < <(fd --follow --no-ignore --hidden --glob --type f --color=never --print0 "$@")
+    readarray -d '' FILES < <(fd --follow --hidden --glob --type f --color=never --print0 "$@")
     for FILE in "${FILES[@]}" ; do
         echo source "\"${FILE}\""
         # shellcheck disable=SC1090,SC2086
