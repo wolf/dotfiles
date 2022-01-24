ba761b5679d5626930691a1cccf02a191b08fea9
Wolf <wolf@zv.cx>
2022-01-24 04:19:39 -0500

    Move back to hand-installed pyenv
    ...as opposed to brew.  Therefor, I need to add pyenv to my path, also
    by hand.

--- bash/.bash_topics.d/python-pyenv.bash
+++ bash/.bash_topics.d/python-pyenv.bash
@@ -1,6 +1,6 @@
-command -v pyenv >/dev/null || return
-
 export PYENV_ROOT="${HOME}/.pyenv"
+export PATH="${PYENV_ROOT}/bin:${PATH}"
+
 eval "$(pyenv init --path)"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
