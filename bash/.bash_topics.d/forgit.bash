75581c9eb23f1220d1dc3664e9af1cfa22a392fd
Wolf <wolf@zv.cx>
2022-01-31 06:16:57 -0500

    Use forgit's built-in aliases
    ...instead of my long hard to type ones.

--- bash/.bash_topics.d/forgit.bash
+++ bash/.bash_topics.d/forgit.bash
@@ -2,23 +2,9 @@
 
 [ -f ~/builds/forgit/forgit.plugin.sh ] || return
 
-forgit_log=zlog
-forgit_diff=zdiff
-forgit_add=zadd
-forgit_reset_head=zreset
-forgit_ignore=zbuild_ignore
-forgit_checkout_file=zcheckout_file
-forgit_checkout_branch=zswitch
-forgit_checkout_commit=zcheckout_commit
-forgit_clean=zclean
-forgit_stash_show=zstash_show
-forgit_cherry_pick=zcherry_pick
-forgit_rebase=zrebase
-forgit_fixup=zfixup
-
 export FORGIT_FZF_DEFAULT_OPTS='--reverse'
 
 # shellcheck disable=SC1090
 source ~/builds/forgit/forgit.plugin.sh
 
-
+export PATH="${PATH}:${FORGIT_INSTALL_DIR}/bin"
