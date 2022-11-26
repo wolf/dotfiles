2446277692a891eb2e7e1c8544588b7da2b52ad3
Wolf <wolf@zv.cx>
2022-11-26 11:54:42 -0500

    Use rbenv instead of rvm from now on

--- bash/dot-bash_topics.d/ruby.bash
+++ bash/dot-bash_topics.d/ruby.bash
@@ -1,6 +1,1 @@
-export RUBYOPT='--disable-did_you_mean'
-
-# Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
-export PATH="$PATH:$HOME/.rvm/bin"
-
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+eval "$(rbenv init - bash)"
