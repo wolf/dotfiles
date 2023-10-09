a71c82d7e4f056b997063786e31659bb8494449a
Wolf <Wolf@genomenon.com>
2023-10-09 16:14:53 -0400

    Use ruff instead of flake8

--- bash/dot-bash_topics.d/python-flake8.bash
+++
@@ -1,9 +0,0 @@
-command -v flake8 >/dev/null || return
-
-function flake8_name() { # flake8_name [<pattern>] : find all the python files whose names match <pattern> in or below . and run them through flake8
-    fd --follow --hidden --type f --extension py "$@" --exec-batch flake8
-}
-
-function flake8_dirty() { # flake8_dirty : like dirty, but run found python files through flake8 instead of list
-    dirty | grep '\.py$' | xargs flake8
-}
