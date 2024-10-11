cf75d516f7b336b03a32a137a75f20cdbec5c5b1
Wolf <wolf@zv.cx>
2024-10-11 13:30:35 -0400

    Add python-uv.bash topic to run different versions of Python

---
+++ bash/dot-bash_topics.d/python-uv.bash
@@ -0,0 +1,7 @@
+alias python3.8="uv run --python=3.8 python3"
+alias python3.9="uv run --python=3.9 python3"
+alias python3.10="uv run --python=3.10 python3"
+alias python3.11="uv run --python=3.11 python3"
+alias python3.12="uv run --python=3.12 python3"
+alias python3.13="uv run --python=3.13 python3"
+alias python3=python3.12
