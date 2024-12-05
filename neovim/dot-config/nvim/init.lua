36185b29bb73c7d23dd08cff8321b5f7458c9c67
Wolf <Wolf@zv.cx>
2024-12-05 09:39:10 -0500

    For NeoVim, enable pyright and treesitter for Python

--- neovim/dot-config/nvim/init.lua
+++ neovim/dot-config/nvim/init.lua
@@ -7,4 +7,12 @@ if vim.fn.has('nvim') == 1 then
     else
         print("hop.nvim is not available")
     end
+
+    require'lspconfig'.pyright.setup{}
+    require'nvim-treesitter.configs'.setup {
+      ensure_installed = { "python" }, -- Ensure Python parser is installed
+      highlight = {
+        enable = true,                -- Enable Treesitter highlighting
+      },
+    }
 end
