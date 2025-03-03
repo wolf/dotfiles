62d87984385ec579c175d0b612a399e4ccd5494a
Wolf <Wolf@zv.cx>
2025-03-03 08:59:33 -0500

    Some fixups for nvim: don't mistype vim; use the right venv

--- bash/dot-bash_topics.d/nvim.bash
+++ bash/dot-bash_topics.d/nvim.bash
@@ -2,6 +2,8 @@ export VISUAL='nvim'
 export EDITOR='nvim -p'
 export FCEDIT='nvim'
 
+alias vim='nvim -p'
+
 function gv() { # gv <pattern> : find matches in or below . and open them in NeoVim's quickfix list
     ${EDITOR} -q <(rg --follow --hidden --smart-case --vimgrep "$@") -c ':copen' -c ':bdelete 1'
 }
