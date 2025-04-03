6df07b11efb47626437356bacb65710fd6ec80d7
Wolf <Wolf@zv.cx>
2025-04-03 15:41:16 -0400

    I'm using Helix (at least for a while) which doesn't understand
    ...and that's all these functions were for.

--- bash/dot-bash_topics.d/darwin/fd.bash
+++
@@ -1,4 +0,0 @@
-function fex() { # fex <glob> : find all the files in or below . whose names match <glob> and open them in an existing $EDITOR
-    # shellcheck disable=SC2086
-    fd --follow --hidden --glob --type f "$@" --exec-batch ${EDITOR} --remote
-}
--- bash/dot-bash_topics.d/darwin/ripgrep.bash
+++
@@ -1,4 +0,0 @@
-function gex() { # gex <regexp> : find files in or below . whose contents somehow match <regexp>, and open them in an existing $EDITOR
-    # shellcheck disable=SC2086
-    rg --follow --hidden --smart-case --files-with-matches -0 "$@" | xargs -0 -o ${EDITOR} --remote
-}
