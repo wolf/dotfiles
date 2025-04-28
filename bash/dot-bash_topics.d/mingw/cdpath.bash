6c288678218f90f2aa960d2dde94b9a32a4f5f93
wolf <wolf@dmp-maps.com>
2025-04-28 10:26:16 -0400

    On Git Bash for Windows, relabeling my Dev Drive to E:

--- bash/dot-bash_topics.d/mingw/cdpath.bash
+++ bash/dot-bash_topics.d/mingw/cdpath.bash
@@ -1,3 +1,3 @@
-export DMP_REPOS_DIR="/d/developer/repos"
+export DMP_REPOS_DIR="/e/developer/repos"
 # DOTFILES_DIR, PROJECTS_DIR, and DROPBOX_DIR are expected to already be defined
 export CDPATH=.:"${DMP_REPOS_DIR}":"${DOTFILES_DIR}":"${PROJECTS_DIR}":"${DROPBOX_DIR}"
