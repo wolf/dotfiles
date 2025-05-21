71dd50650a8161e56319fe0e5946133f96544027
Wolf <Wolf@zv.cx>
2025-05-21 05:47:12 -0400

    Document Windows behavior of `deduplicate_path.py`

--- bash/dot-bash_tools.bin/deduplicate_path.py
+++ bash/dot-bash_tools.bin/deduplicate_path.py
@@ -2,16 +2,14 @@
 """
 $PATH may contain (useless) duplicates.  Running `eval "$(deduplicate_path.py)"` eliminates them.
 
-To be run once as the very last thing that happens in the Bash startup sequence.
+To be run once as the very last thing that happens in the Bash (and this is a Bash-only script) startup sequence.
 """
 
 import os
 import platform
 from pathlib import Path, PureWindowsPath
 
-original_paths = os.getenv(
-    "PATH"
-)  # Technically this can return `None`.  I handle it, but it will never really happen.
+original_paths = os.getenv("PATH")  # Technically this can return `None`.  I handle it, but it will never really happen.
 output_paths = ""
 
 _is_windows = platform.system() == "Windows"
@@ -20,6 +18,7 @@ _paths_already_present = set()
 
 
 def is_new_path(path: str) -> bool:
+    """`True` if we've seen `path` before; `False` otherwise"""
     global _paths_already_present
     if path in _paths_already_present:
         return False
@@ -28,24 +27,46 @@ def is_new_path(path: str) -> bool:
 
 
 def posix_path(path: str) -> Path:
+    """
+    Convert a platform-dependent string into a `pathlib.Path`
+
+    Why is this hard?  Git Bash for Windows is a special case.  WSL acts exactly like any other Linux. It is not
+    special.  I don't run any other Windows-specific Bash environments, so Git Bash for Windows is all I have to worry
+    about.
+
+    Here's the weird thing Git Bash for Windows does: within a Bash session, if you `echo $PATH`, you will what looks
+    like a POSIX-style path.  It will have forward slashes, `/`.  Each path will be separated by a colon, `:`.  And
+    no path in the list will start with drive letter, e.g., you won't see `E:`, you'll see `/e/...`.  This is fiction.
+    If you get `PATH` from with Python (go ahead and test with IPython) you'll see what you see in Windows system
+    environment variables dialog.  `PATH` is a list of directories separated by semicolons, `;`.  The slashes go the
+    other way.  And any invidiual path that start from a specific volume begins with a drive letter, e.g., `E:`.
+
+    Git Bash for Windows automatically translates between these two different representations of `PATH`.  On Windows,
+    my `original_paths` looks like Windows paths.  Converting a Windows path from `str` form into a `PureWindowsPath`
+    fixes the slashes, but if there's a drive letter, I have to handle that myself.
+
+    Could I have converted all of `original_paths` at once?  Yes.  I think it would be more complicated and probably use
+    regular expressions.  `$PATH` doesn't have a ton of elements.  Simple is fast enough.
+    """
     if _is_windows:
         p = PureWindowsPath(path)
         if not p.drive:
             return Path(p)
         drive = p.drive[0].lower()
-        everything_else = str(p)[len(drive) + 1:]
+        everything_else = str(p)[len(drive) + 1 :]
         return Path(f"/{drive}/{everything_else}")
     else:
         return Path(path)
 
 
 if original_paths is not None:
-    deduplicated_paths = [
-        posix_path(path) for path in original_paths.split(_path_separator) if is_new_path(path)
-    ]
+    deduplicated_paths = [posix_path(path) for path in original_paths.split(_path_separator) if is_new_path(path)]
+
+    # Because the result will always be handled inside Bash, I produce the well-known, `:` separated, forward slash
+    # version of `$PATH`.
     output_paths = ":".join(
         path.as_posix() for path in deduplicated_paths[1:]
-    )  # Trim the path added by `uv` just to run this script.
+    )  # Using the slice `[1:]` trims the path `uv` added just to run this script.
 else:
     output_paths = original_paths
 
