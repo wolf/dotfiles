73c8bb9bea73d47b49532543e5663b2e8245fe50
Wolf <Wolf@zv.cx>
2025-05-28 15:12:49 -0400

    Fix up fix_path_separators in posix_path

--- bash/dot-bash_tools.bin/posix_path.py
+++ bash/dot-bash_tools.bin/posix_path.py
@@ -1,20 +1,27 @@
-from os import PathLike
 import platform
+import re
+from os import PathLike
 from pathlib import PurePosixPath, PureWindowsPath
 
 
+# Calculated once, when this module is imported
 _is_windows = platform.system() == "Windows"
+_consecutive_windows_slashes_pat = re.compile(r'\\+')
+_consecutive_posix_slashes_pat = re.compile(r'/+')
 
 
 def fix_path_separators(path: PathLike) -> PurePosixPath:
-    return PurePosixPath(str(path).replace('\\+', '/').replace('/+', '/'))
+    broken_path = str(path)
+    windows_slashes_fixed = re.sub(_consecutive_windows_slashes_pat, '/', broken_path)
+    consecutive_posix_slashes_fixed = re.sub(_consecutive_posix_slashes_pat, '/', windows_slashes_fixed)
+    return PurePosixPath(consecutive_posix_slashes_fixed)
 
 
 def posix_path(path: PathLike) -> PurePosixPath:
     """
-    Convert a platform-dependent string into a `pathlib.Path`
+    Convert a platform-dependent string into a `pathlib.PurePosixPath`, formatted correctly
 
-    Why is this hard?  Git Bash for Windows is a special case.  WSL acts exactly like any other Linux. It is not
+    Why is this hard?  Git Bash for Windows is the special case.  WSL acts exactly like any other Linux. It is not
     special.  I don't run any other Windows-specific Bash environments, so Git Bash for Windows is all I have to worry
     about.
 
@@ -29,10 +36,14 @@ def posix_path(path: PathLike) -> PurePosixPath:
     my `original_paths` looks like Windows paths.  Converting a Windows path from `str` form into a `PureWindowsPath`
     fixes the slashes, but if there's a drive letter, I have to handle that myself.
 
-    Could I have converted all of `original_paths` at once?  Yes.  I think it would be more complicated and probably use
-    regular expressions.  `$PATH` doesn't have a ton of elements.  Simple is fast enough.
+    In fact, depending on how you call this function, `path` could be a `Path`, a `PurePosixPath`, or a `PureWindowsPath`,
+    and it could look a lot of different ways.  It could be a valid posix-style path already, e.g., '/home/wolf/bin'.
+    It could be a valid windows-style path, e.g., r'C:\\Users\\Wolf\\bin'.  It might be some weird combination.  This
+    function gets you part way to right thing, if you're on Windows and therefore the path is non-posix-compliant.  It
+    fixes the drive part, dropping the `:` that is part of that.  Then `fix_path_separators` fixes everything else and
+    returns a `PurePosixPath`.
     """
-    if _is_windows:
+    if _is_windows or type(path) is PureWindowsPath:
         p = PureWindowsPath(path)
         if not p.drive:
             return fix_path_separators(p)
