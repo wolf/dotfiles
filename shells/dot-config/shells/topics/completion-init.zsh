0cc1bbc42342f29f98704da2c5e5d41aced85d19
Wolf <Wolf@zv.cx>
2025-06-28 10:28:01 -0400

    Make Zsh autocomplete arguments to aliases

--- shells/dot-config/shells/topics/completion-init.zsh
+++ shells/dot-config/shells/topics/completion-init.zsh
@@ -2,6 +2,9 @@
 autoload -Uz compinit
 compinit
 
+# Complete aliases (e.g., `ll`) as the original command (e.g., for `ll`, as though calling `eza`)
+setopt completealiases
+
 # Add Homebrew's Zsh completion directory to fpath
 if command -v brew >/dev/null 2>&1; then
     fpath=("$(brew --prefix)/share/zsh/site-functions" $fpath)
