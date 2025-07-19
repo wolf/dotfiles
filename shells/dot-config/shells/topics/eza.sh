f868a3b96423961cf6843b89bfc07ade40b7aeee
Wolf <Wolf@zv.cx>
2025-07-19 18:37:23 -0400

    Give eza a theme

--- shells/dot-config/shells/topics/eza.sh
+++ shells/dot-config/shells/topics/eza.sh
@@ -1,5 +1,11 @@
 command -v eza >/dev/null 2>&1 || return
 
+unset LS_COLORS
+unset EXA_COLORS
+unset EZA_COLORS
+
+export EZA_CONFIG_DIR="${HOME}/.config/eza"
+
 alias ls=eza
 alias ll="eza -alF"
 
