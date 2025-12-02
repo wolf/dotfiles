2cfd9f59a2d04cb3845ab13c5b1f121a8ba60f59
Wolf <Wolf@zv.cx>
2025-12-02 11:47:00 -0500

    Add the parent directory to my personal and work repo dirs to CDPATH
    That way, for instance, I can `cd dmp` and instantly see all DMP projects
    instead of `cd siloc/..`.

--- shells/dot-config/shells/topics/cdpath.sh
+++ shells/dot-config/shells/topics/cdpath.sh
@@ -1,12 +1,13 @@
 # Environment variables to be used in `$CDPATH` and anywhere else that needs them.
-export WOLF_REPOS_DIR="${HOME}/develop/wolf"
-export DMP_REPOS_DIR="${HOME}/develop/dmp"
+export REPOS_DIR="${HOME}/develop"
+export WOLF_REPOS_DIR="${REPOS_DIR}/wolf"
+export DMP_REPOS_DIR="${REPOS_DIR}/dmp"
 export DROPBOX_DIR="${HOME}/Dropbox"
 export PROJECTS_DIR="${DROPBOX_DIR}/projects"
 export DOTFILES_DIR="${WOLF_REPOS_DIR}/dotfiles"
 
 # This is for typing directory names at the prompt, therefore, this topic only needs to be sourced for,
 # interactive shells.
-export CDPATH=.:"${WOLF_REPOS_DIR}":"${DMP_REPOS_DIR}":"${PROJECTS_DIR}":"${DROPBOX_DIR}"
+export CDPATH=.:"${WOLF_REPOS_DIR}":"${DMP_REPOS_DIR}":"${REPOS_DIR}":"${PROJECTS_DIR}":"${DROPBOX_DIR}"
 
 show_cdpath() { echo "${CDPATH}" | tr ':' '\n'; }  # show_cdpath : display `$CDPATH`, one path per line
--- shells/dot-config/shells/topics/mingw/cdpath.sh
+++ shells/dot-config/shells/topics/mingw/cdpath.sh
@@ -1,9 +1,10 @@
 # Environment variables to be used in `$CDPATH` and anywhere else that needs them.
-export WOLF_REPOS_DIR="/e/develop/wolf"
-export DMP_REPOS_DIR="/e/develop/dmp"
+export REPOS_DIR="/e/develop"
+export WOLF_REPOS_DIR="${REPOS_DIR}/wolf"
+export DMP_REPOS_DIR="${REPOS_DIR}/dmp"
 export DOTFILES_DIR="${WOLF_REPOS_DIR}/dotfiles"
 
 # `$PROJECTS_DIR` and `$DROPBOX_DIR` are expected to already exist
 # This is for typing directory names at the prompt, therefore, this topic only needs to be sourced for
 # interactive shells.
-export CDPATH=.:"${WOLF_REPOS_DIR}":"${DMP_REPOS_DIR}":"${PROJECTS_DIR}":"${DROPBOX_DIR}"
+export CDPATH=.:"${WOLF_REPOS_DIR}":"${DMP_REPOS_DIR}":"${REPOS_DIR}":"${PROJECTS_DIR}":"${DROPBOX_DIR}"
