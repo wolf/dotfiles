845ad16460811e70b50da73f02c2ccdc68a5f1b9
Wolf <Wolf@zv.cx>
2025-12-22 16:53:52 -0500

    `build_helix` needs to fetch and build all the grammars, too
    ...and if the general `build_helix` starts by deleting the existing `hx`,
    then we don't need to specialize on Git Bash for Windows.

--- shells/dot-config/shells/topics/build-helix.sh
+++ shells/dot-config/shells/topics/build-helix.sh
@@ -3,13 +3,19 @@ export HELIX_BUILD_DIR="${WOLF_REPOS_DIR}/helix"
 export HELIX_DEFAULT_RUNTIME="${HELIX_BUILD_DIR}/runtime"
 
 build_helix() {
+  rm -f "$(which hx)"
+
   if command -v direnv >/dev/null 2>&1; then
     direnv block "${HELIX_BUILD_DIR}/.envrc"
   fi
+
   cd "${HELIX_BUILD_DIR}"
   git switch master
   git fetch --all
   git pull
   git pull upstream master
   cargo install --path helix-term --locked
+
+  hx --grammar fetch
+  hx --grammar build
 }
