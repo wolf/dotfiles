ece141a8cd1bca79e66626c009e24b1434112772
Wolf <Wolf@zv.cx>
2025-12-23 11:46:01 -0500

    Just to be double-sure this works on Windows

--- shells/dot-config/shells/topics/build-helix.sh
+++ shells/dot-config/shells/topics/build-helix.sh
@@ -4,7 +4,7 @@ export HELIX_DEFAULT_RUNTIME="${HELIX_BUILD_DIR}/runtime"
 
 build_helix() {
   # Might not be there, might not be `$(which hx)`, might be `hx.exe` (need to check that works)
-  rm -f "~/.cargo/bin/hx"
+  rm -f "~/.cargo/bin/hx{,.exe}"
 
   if command -v direnv >/dev/null 2>&1; then
     direnv block "${HELIX_BUILD_DIR}/.envrc"
