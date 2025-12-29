e8e7b3039a60af26f71bb6a2f90b26974c156f61
Wolf <Wolf@zv.cx>
2025-12-29 16:18:51 -0500

    Comment the origin of my `direnv` path unmangling code

--- shells/dot-config/shells/topics/mingw/direnv.bash
+++ shells/dot-config/shells/topics/mingw/direnv.bash
@@ -1,5 +1,7 @@
 command -v direnv >/dev/null 2>&1 || return
 
+# Started from the solution I found at: https://eighty-twenty.org/2024/01/04/direnv-path-unmangling-on-windows
+
 export _unmangle_direnv_names='PATH PYTHONPATH'
 _unmangle_direnv_paths() {
   for k in $_unmangle_direnv_names; do
