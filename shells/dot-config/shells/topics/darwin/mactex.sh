63c8c1c54e920f5c59209577a7802fb2e78e6d00
Wolf <Wolf@zv.cx>
2026-01-06 14:59:14 -0500

    Make a few things distinguished from the background in my Helix Alabaster theme
    ...and I also don't really use MacTex all that much, and it's messing up `$PATH`;
    so goodbye.

--- shells/dot-config/shells/topics/darwin/mactex.sh
+++
@@ -1,5 +0,0 @@
-[ -x /usr/libexec/path_helper ] >/dev/null 2>&1 || return
-
-# This eval modifies `$PATH`, and it makes available scripts and tools that might be used non-interactively.
-# Therefore, this topic should _always_ be sourced.
-eval "$(/usr/libexec/path_helper)"
