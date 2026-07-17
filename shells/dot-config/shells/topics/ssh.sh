f5882f663d5ea4333dbc8eb08d24d9cc387fd64f
Wolf <Wolf@zv.cx>
2026-07-17 12:20:48 -0400

    A better implementation of my `hosts` command (still a shell function)

--- shells/dot-config/shells/topics/ssh.sh
+++ shells/dot-config/shells/topics/ssh.sh
@@ -1,5 +1,8 @@
-function hosts() {  # hosts : list Hosts configured in `~/.ssh/config`
-    if [ -f "${HOME}/.ssh/config" ] ; then
-        grep -e '^Host' "${HOME}/.ssh/config"
+function hosts() {  # hosts : list Hosts configured in `~/.ssh/config`, following Include directives
+    if command -v ssh-hosts >/dev/null 2>&1 ; then
+        ssh-hosts "$@"
+    else
+        echo "hosts: ssh-hosts not installed; run: cargo install --path ~/develop/wolf/ssh-hosts" >&2
+        return 127
     fi
 }
