b49778a7b7a4be57b6742986afa97ce9566707a4
Wolf <Wolf@zv.cx>
2026-07-21 10:33:29 -0400

    Homebrew: don't --ask during upgrade

--- shells/dot-config/shells/topics/darwin/brew.sh
+++ shells/dot-config/shells/topics/darwin/brew.sh
@@ -1,4 +1,5 @@
 if [ -f /opt/homebrew/bin/brew ]; then
     eval "$(/opt/homebrew/bin/brew shellenv)"
     export PATH="/opt/homebrew/opt/gnu-getopt/bin:${PATH}:/opt/homebrew/opt/coreutils/libexec/gnubin"
+    export HOMEBREW_NO_ASK=1
 fi
--- shells/dot-config/shells/topics/linux/brew.sh
+++ shells/dot-config/shells/topics/linux/brew.sh
@@ -1,3 +1,4 @@
 if [ -f /home/linuxbrew/.linuxbrew/bin/brew ] ; then
     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+    export HOMEBREW_NO_ASK=1
 fi
